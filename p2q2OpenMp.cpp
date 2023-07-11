6c941dfa3b1cac17f846ce1c2703f20d21d34bb1
jason6356 <xiaodemon6536@gmail.com>
2023-07-11 09:54:56 +0800

    p2

--- p2q2OpenMp.cpp
+++ p2q2OpenMp.cpp
@@ -2,7 +2,7 @@
 #include "omp.h"
 #include <stdio.h>
 #define PADS 8
-#define NUM_THREADS 1
+#define NUM_THREADS 16
 
 static long num_steps = 100000;
 double step;
@@ -11,13 +11,13 @@ double step;
 int main()
 {
     //double x, pi, sum = 0.0;
-    double pi, sum;
+    double pi, sum = 0.0;
     omp_set_num_threads(NUM_THREADS);
     double time = omp_get_wtime();
     step = 1.0 / (double)num_steps;
 
-#pragma omp reduction(+:sum)
     //tells the compiler to perform a reduction operation on the variable sum
+#pragma omp parallel reduction(+ : sum)
     {
         int i, id, nth;
         double x;
@@ -27,14 +27,16 @@ int main()
         {
             x = (i + 0.5) * step;
 
-            sum+= 4.0 / (1.0 + x * x);
-            printf("Thread_ID = %d\n Sum = %f\n", id, sum);
+            sum += 4.0 / (1.0 + x * x);
         }
+        //printf("ID = %d\nSum = %f\n", id,sum);
     }
     double end_time = omp_get_wtime();
     pi = sum * step;
-    printf("%f\n", pi);
+    printf("%lf\n", sum);
+    printf("%lf\n", pi);
     printf("Work took %f seconds\n", end_time - time);
+
 
     return 0;
 }
